3662c37c1932aade2ca8c73a7c0ed638b2161269
Andrew H Wang <aw1605@gmail.com>
2018-10-16 13:51:27 -0700

    Updated with (hopefully) veering correction

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDriveTrain.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDriveTrain.java
@@ -46,13 +46,31 @@ public class AutonomousDriveTrain extends DriveTrain {
 
         runtime.reset();
         driveFrontLeft.setPower(Math.abs(power));
-        driveFrontRight.setPower(Math.abs(power));
+        driveFrontRight.setPower(-Math.abs(power));
         driveBackLeft.setPower(Math.abs(power));
-        driveBackRight.setPower(Math.abs(power));
+        driveBackRight.setPower(-Math.abs(power));
+
+        while (leftEncoder.isBusy() && rightEncoder.isBusy()){
+            if(leftEncoder.getCurrentPosition()-rightEncoder.getCurrentPosition() > 10) {
+                driveFrontLeft.setPower(Math.abs(power) * 0.9);
+                driveFrontRight.setPower(-Math.abs(power) * 1.1);
+                driveBackLeft.setPower(Math.abs(power) * 0.9);
+                driveBackRight.setPower(-Math.abs(power) * 1.1);
+            } else if (rightEncoder.getCurrentPosition() - leftEncoder.getCurrentPosition() > 10) {
+                driveFrontLeft.setPower(Math.abs(power) * 1.1);
+                driveFrontRight.setPower(-Math.abs(power) * 0.9);
+                driveBackLeft.setPower(Math.abs(power) * 1.1);
+                driveBackRight.setPower(-Math.abs(power) * 0.9);
+            }
+        }
 
         driveFrontLeft.setPower(0);
         driveFrontRight.setPower(0);
         driveBackLeft.setPower(0);
         driveBackRight.setPower(0);
-    } 
+
+        leftEncoder.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightEncoder.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
 }
