d484f1f84246b5144fb9b0097d14a7bbfbbc4c65
Andrew H Wang <aw1605@gmail.com>
2018-10-18 18:51:08 -0700

    Ready to test autonomous drive forward/back

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDriveTrain.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDriveTrain.java
@@ -10,11 +10,9 @@ public class AutonomousDriveTrain extends DriveTrain {
 
     private ElapsedTime runtime = new ElapsedTime();
 
-    static final double COUNT_PER_MOTOR_REV     = 1680;
-    static final double GEAR_REDUCTION          = 0.5;
+    static final double PPR                     = 90;
     static final double WHEEL_DIAMETER_INCHES   = 4.0;
-    static final double COUNTS_PER_INCH         = (COUNT_PER_MOTOR_REV * GEAR_REDUCTION /
-            WHEEL_DIAMETER_INCHES / Math.PI);
+    static final double COUNTS_PER_INCH         = PPR * 1 / (WHEEL_DIAMETER_INCHES * Math.PI);
 
     static final double DRIVE_SPEED             = 0.3;
     static final double TURN_SPEED              = 0.5;
@@ -29,9 +27,42 @@ public class AutonomousDriveTrain extends DriveTrain {
         strafeEncoder.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
-    public void encoderDrive(double power, double distance, double timeout) {
+    public void encoderDrive(double power, double distance, double timeout) {//distance must be positive; goes forward distance inches
+        if (distance < 0) {
+            throw new IllegalArgumentException("Distance must be negative");
+        }
 
-        int newLeftTarget = leftEncoder.getCurrentPosition() + (int)(distance * COUNTS_PER_INCH);
+        int targetEncoderPosition = (int) (distance * COUNTS_PER_INCH);
+
+        initialize();//resets encoders
+
+        while (Math.abs(leftEncoder.getCurrentPosition()) < targetEncoderPosition || Math.abs(rightEncoder.getCurrentPosition()) < targetEncoderPosition) {
+            if (leftEncoder.getCurrentPosition() - rightEncoder.getCurrentPosition() > 0) {
+                runMotor(driveFrontRight, power);
+                runMotor(driveFrontLeft, power * (1 - .01 * leftEncoder.getCurrentPosition() - rightEncoder.getCurrentPosition()));
+                runMotor(driveBackRight, power);
+                runMotor(driveBackLeft, power * (1 - .01 * leftEncoder.getCurrentPosition() - rightEncoder.getCurrentPosition()));
+            } else if (rightEncoder.getCurrentPosition() - leftEncoder.getCurrentPosition() > 0) {
+                runMotor(driveFrontRight, power * (1 - .01 * rightEncoder.getCurrentPosition() - leftEncoder.getCurrentPosition()));
+                runMotor(driveFrontLeft, power);
+                runMotor(driveBackRight, power * (1 - .01 * rightEncoder.getCurrentPosition() - leftEncoder.getCurrentPosition()));
+                runMotor(driveBackLeft, power);
+            } else {
+                runMotor(driveFrontRight,power);
+                runMotor(driveFrontLeft, power);
+                runMotor(driveBackRight, power);
+                runMotor(driveBackLeft, power);
+            }
+        }
+
+        runMotor(driveFrontLeft,0);
+        runMotor(driveFrontRight,0);
+        runMotor(driveBackLeft, 0);
+        runMotor(driveBackRight, 0);
+
+
+
+        /*int newLeftTarget = leftEncoder.getCurrentPosition() + (int)(distance * COUNTS_PER_INCH);
         int newRightTarget = rightEncoder.getCurrentPosition() + (int)(distance * COUNTS_PER_INCH);
 
         leftEncoder.setTargetPosition(newLeftTarget);
@@ -44,7 +75,7 @@ public class AutonomousDriveTrain extends DriveTrain {
         driveBackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         driveBackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);*/
 
-        runtime.reset();
+        /*runtime.reset();
         driveFrontLeft.setPower(Math.abs(power));
         driveFrontRight.setPower(-Math.abs(power));
         driveBackLeft.setPower(Math.abs(power));
@@ -70,7 +101,7 @@ public class AutonomousDriveTrain extends DriveTrain {
         driveBackRight.setPower(0);
 
         leftEncoder.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightEncoder.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightEncoder.setMode(DcMotor.RunMode.RUN_USING_ENCODER);*/
 
     }
 }
